dbc7578bc450841dd6cdf08e489f1f7a4498a2d6
fxb-HQ007 <516668646@qq.com>
2022-04-18 18:53:31 +0800

    tijiao

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,17 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+
 public class HelloController {
-    @RequestMapping
+    @GetMapping("/index")
     public String index() {
-        System.out.println("hello.springboot的第一个controller");
         return "index";
     }
 
